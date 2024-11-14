05113589722ee637360396f5bf52ec5ce070b975
Bhushan Harpade <bhushanharpade13@gmail.com>
2024-11-14 10:37:15 +0530

    Initialized new  Migration

---
+++ WebAPI/Migrations/WeatherDBMigrations/20241114045758_NewWeatherDB.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations.WeatherDBMigrations
+{
+    /// <inheritdoc />
+    public partial class NewWeatherDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherData",
+                columns: table => new
+                {
+                    WeatherId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    Temp = table.Column<float>(type: "REAL", nullable: false),
+                    Humidity = table.Column<int>(type: "INTEGER", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherData", x => x.WeatherId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherData");
+        }
+    }
+}
